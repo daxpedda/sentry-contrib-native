746e52545d6d8603487d3689cd326411bb4e2498
Jake Shadle <jake.shadle@embark-studios.com>
2020-06-15 14:55:38 +0200

    Remove debug code

--- build.rs
+++ build.rs
@@ -12,10 +12,6 @@
 use std::{env, path::Path};
 
 fn main() {
-    for (k, v) in env::vars() {
-        println!("{} = {}", k, v);
-    }
-
     if let Ok(handler) = env::var("DEP_SENTRY_NATIVE_HANDLER") {
         if cfg!(feature = "copy-handler") {
             let out_dir = env::var("OUT_DIR").expect("out dir not set");
@@ -36,11 +32,6 @@ fn main() {
                     .expect("handler doesn't have a file name"),
             );
 
-            println!(
-                "cargo:warning=\"Copying {} to {}\"",
-                handler.display(),
-                bin_path.display()
-            );
             std::fs::copy(&handler, &bin_path).expect("failed to copy sentry crash handler");
         } else {
             println!("cargo:rustc-env=SENTRY_HANDLER={}", handler);

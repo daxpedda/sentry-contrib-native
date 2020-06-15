05cd170369f8c562af13ae3a1a76b1d6274da04e
Jake Shadle <jake.shadle@embark-studios.com>
2020-06-15 14:15:58 +0200

    Add copy-handler feature

--- build.rs
+++ build.rs
@@ -16,8 +16,34 @@ fn main() {
         println!("{} = {}", k, v);
     }
 
-    if let Ok(p) = env::var("DEP_SENTRY_NATIVE_HANDLER") {
-        println!("cargo:rustc-env=SENTRY_HANDLER={}", p);
-        println!("cargo:SENTRY_HANDLER={}", p);
+    if let Ok(handler) = env::var("DEP_SENTRY_NATIVE_HANDLER") {
+        if cfg!(feature = "copy-handler") {
+            let out_dir = env::var("OUT_DIR").expect("out dir not set");
+
+            let out_dir = Path::new(&out_dir);
+            let bin_dir = out_dir
+                .parent()
+                .unwrap()
+                .parent()
+                .unwrap()
+                .parent()
+                .unwrap();
+
+            let handler = Path::new(&handler);
+            let bin_path = bin_dir.join(
+                handler
+                    .file_name()
+                    .expect("handler doesn't have a file name"),
+            );
+
+            println!(
+                "cargo:warning=\"Copying {} to {}\"",
+                handler.display(),
+                bin_path.display()
+            );
+            std::fs::copy(&handler, &bin_path).expect("failed to copy sentry crash handler");
+        } else {
+            println!("cargo:rustc-env=SENTRY_HANDLER={}", handler);
+        }
     }
 }

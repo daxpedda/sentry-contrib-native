737c405edc02c1a24f45dfcc6855807e73d86e01
daxpedda <daxpedda@gmail.com>
2020-07-01 00:01:20 +0000

    Deploying to gh-pages from  @ 99c2612b219aa3b3f766ce8659789a277cbf0b94 🚀

--- master/doc/source-files.js
+++ master/doc/source-files.js
@@ -1,5 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["dylib"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["sentry_contrib_native"] = {"name":"","files":["before_send.rs","breadcrumb.rs","event.rs","ffi.rs","lib.rs","logger.rs","object.rs","options.rs","panic.rs","transport.rs","user.rs","value.rs"]};
 sourcesIndex["sentry_contrib_native_sys"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();

02a3a088fc46b145abfb3498f08f4b86519b8bc6
daxpedda <daxpedda@gmail.com>
2020-07-06 18:13:44 +0200

    Expand `crash` test.

--- tests/crash.rs
+++ tests/crash.rs
@@ -12,8 +12,118 @@
 mod util;
 
 use anyhow::Result;
+use serde_json::Value;
+//use sha1::{Digest, Sha1};
+use std::{
+    //fs,
+    path::{Path, PathBuf},
+};
+
+fn lib_path() -> PathBuf {
+    let mut path = PathBuf::from(env!("OUT_DIR"))
+        .parent()
+        .and_then(Path::parent)
+        .and_then(Path::parent)
+        .unwrap()
+        .join("deps");
+
+    #[cfg(target_os = "linux")]
+    {
+        path = path.join("libdylib.so");
+    }
+    #[cfg(target_os = "macos")]
+    {
+        path = path.join("libdylib.dylib");
+    }
+    #[cfg(target_os = "windows")]
+    {
+        path = path.join("dylib.dll");
+    }
+
+    path
+}
 
 #[tokio::test(threaded_scheduler)]
 async fn crash() -> Result<()> {
-    util::external_events_success(vec![("crash".into(), |_| {})]).await
+    util::external_events_success(vec![("crash".into(), |event| {
+        // options
+        {
+            let release = event.release.unwrap();
+
+            assert_eq!("1.0", event.tags.get("release").unwrap());
+            assert_eq!("1.0", release.short_version.unwrap());
+            assert_eq!("1.0", release.version.unwrap());
+            assert_eq!("1.0", release.version_info.as_ref().unwrap().description);
+            assert_eq!(
+                "1.0",
+                release.version_info.unwrap().version.get("raw").unwrap()
+            );
+
+            assert_eq!("production", event.tags.get("environment").unwrap());
+
+            assert_eq!("release-pgo", event.dist.unwrap());
+            assert_eq!("release-pgo", event.tags.get("dist").unwrap());
+
+            /*let attachment = event.attachments.get(0).unwrap();
+            let content = fs::read_to_string("tests/res/attachment.txt").unwrap();
+            let hash = hex::encode(Sha1::digest(content.as_bytes()));
+            assert_eq!("attachment.txt", attachment.name);
+            assert_eq!(hash, attachment.sha1);
+            assert_eq!(content.len(), attachment.size);*/
+        }
+
+        // breadcrumb
+        {
+            let breadcrumb = event
+                .entries
+                .get("breadcrumbs")
+                .and_then(|v| v.get("values"))
+                .and_then(Value::as_array)
+                .and_then(|v| v.get(0))
+                .and_then(Value::as_object)
+                .unwrap();
+            assert_eq!(
+                Some("test type"),
+                breadcrumb.get("type").and_then(Value::as_str)
+            );
+            assert_eq!(
+                Some("test message"),
+                breadcrumb.get("message").and_then(Value::as_str)
+            );
+        }
+
+        // dylib
+        {
+            let libs = event
+                .entries
+                .get("debugmeta")
+                .and_then(|v| v.get("images"))
+                .and_then(Value::as_array)
+                .unwrap();
+            assert!(libs
+                .iter()
+                .any(|v| v.get("code_file").and_then(Value::as_str).unwrap()
+                    == lib_path().to_str().unwrap()));
+        }
+
+        // tag
+        assert_eq!("test", event.tags.get("test-tag").unwrap());
+
+        // extra
+        assert_eq!("test", event.context.get("test tag").unwrap());
+
+        // context
+        {
+            let context = event.contexts.get("test context").unwrap();
+            assert_eq!("os", context.r#type);
+            assert_eq!("Redox", context.get("name").unwrap());
+        }
+
+        // transcation
+        assert_eq!("test transaction", event.tags.get("transaction").unwrap());
+
+        // level
+        assert_eq!("fatal", event.tags.get("level").unwrap());
+    })])
+    .await
 }
--- tests/res/crash.rs
+++ tests/res/crash.rs
@@ -1,18 +1,43 @@
-use anyhow::Result;
-use sentry::{Options, User};
-use sentry_contrib_native as sentry;
-use std::{
-    io::{self, Read},
-    ptr,
-};
-
 #[cfg(feature = "custom-transport")]
 #[path = "../util/custom_transport.rs"]
 #[rustfmt::skip]
 mod custom_transport;
 
+use anyhow::Result;
 #[cfg(feature = "custom-transport")]
 use custom_transport::Transport;
+use libloading::{Library, Symbol};
+use sentry::{Breadcrumb, Options, User};
+use sentry_contrib_native as sentry;
+use std::{
+    io::{self, Read},
+    path::{Path, PathBuf},
+    ptr,
+};
+
+fn lib_path() -> PathBuf {
+    let mut path = PathBuf::from(env!("OUT_DIR"))
+        .parent()
+        .and_then(Path::parent)
+        .and_then(Path::parent)
+        .unwrap()
+        .join("deps");
+
+    #[cfg(target_os = "linux")]
+    {
+        path = path.join("libdylib.so");
+    }
+    #[cfg(target_os = "macos")]
+    {
+        path = path.join("libdylib.dylib");
+    }
+    #[cfg(target_os = "windows")]
+    {
+        path = path.join("dylib.dll");
+    }
+
+    path
+}
 
 #[tokio::main(threaded_scheduler)]
 async fn main() -> Result<()> {
@@ -20,17 +45,47 @@ async fn main() -> Result<()> {
 
     let mut options = Options::new();
     options.set_debug(true);
+    options.set_release("1.0");
+    options.set_environment("production");
+    options.set_distribution("release-pgo");
+    /*options.add_attachment(
+        "test attachment",
+        "C:/rust/sentry-contrib-native/tests/res/attachment.txt",
+    );*/
     #[cfg(feature = "custom-transport")]
     options.set_transport(Transport::new);
     let _shutdown = options.init()?;
 
-    let mut buffer = [0; 16];
-    io::stdin().read_exact(&mut buffer)?;
-    let id = hex::encode(buffer);
+    // breadcrumb
+    Breadcrumb::new(Some("test type".into()), Some("test message".into())).add();
 
-    let mut user = User::new();
-    user.insert("id", id);
-    user.set();
+    // dylib
+    let lib = Library::new(lib_path()).unwrap();
+    let func: Symbol<extern "C" fn() -> bool> = unsafe { lib.get(b"test\0") }.unwrap();
+    assert_eq!(true, func());
+
+    // tag
+    sentry::set_tag("test-tag", "test");
+
+    // extra
+    sentry::set_extra("test tag", "test");
+
+    // context
+    sentry::set_context("test context", vec![("type", "os"), ("name", "Redox")]);
+
+    // transaction
+    sentry::set_transaction("test transaction");
+
+    // user
+    {
+        let mut buffer = [0; 16];
+        io::stdin().read_exact(&mut buffer)?;
+        let id = hex::encode(buffer);
+
+        let mut user = User::new();
+        user.insert("id", id);
+        user.set();
+    }
 
     unsafe {
         *ptr::null_mut() = true;

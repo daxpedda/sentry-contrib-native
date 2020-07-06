63215885749bb599c37da1d7cd106726055f0d05
daxpedda <daxpedda@gmail.com>
2020-07-06 14:18:42 +0200

    Expand `panic` test.

--- tests/panic.rs
+++ tests/panic.rs
@@ -14,5 +14,13 @@ use anyhow::Result;
 
 #[tokio::test(threaded_scheduler)]
 async fn panic() -> Result<()> {
-    util::external_events_success(vec![("panic".into(), |_| {})]).await
+    util::external_events_success(vec![("panic".into(), |event| {
+        assert_eq!(
+            "panicked at 'test panic', tests/res/panic.rs:32:5",
+            event.title
+        );
+        assert_eq!(Some(5), event.context.get("column").unwrap().as_i64());
+        assert_eq!(Some(32), event.context.get("line").unwrap().as_i64());
+    })])
+    .await
 }

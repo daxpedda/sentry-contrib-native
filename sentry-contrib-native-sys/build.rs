20267e4e3ac869c4dba05f893a66e433db4d5d66
Jake Shadle <jake.shadle@embark-studios.com>
2020-07-14 00:39:16 +0200

    Add backend selection

--- sentry-contrib-native-sys/build.rs
+++ sentry-contrib-native-sys/build.rs
@@ -21,9 +21,54 @@ use std::{
     path::{Path, PathBuf},
 };
 
+#[derive(Copy, Clone)]
+enum Backend {
+    Inproc,
+    Crashpad,
+    Breakpad,
+    None,
+}
+
+fn get_target_default(target_os: &str) -> Backend {
+    match target_os {
+        "windows" | "macos" => Backend::Crashpad,
+        "linux" => Backend::Breakpad,
+        "android" => Backend::Inproc,
+        _ => Backend::None,
+    }
+}
+
+/// Gets the backend we want to use, see https://github.com/getsentry/sentry-native#compile-time-options
+/// for details
+fn get_backend(target_os: &str) -> Backend {
+    if cfg!(feature = "backend-none") {
+        return Backend::None;
+    }
+
+    if cfg!(feature = "backend-crashpad")
+        && (target_os == "macos" || target_os == "windows" || target_os == "linux")
+    {
+        return Backend::Crashpad;
+    }
+
+    if cfg!(feature = "backend-breakpad") && (target_os == "windows" || target_os == "linux") {
+        return Backend::Breakpad;
+    }
+
+    if cfg!(feature = "backend-inproc") {
+        return Backend::Inproc;
+    }
+
+    get_target_default(target_os)
+}
+
 fn main() -> Result<()> {
+    let target_os = env::var("CARGO_CFG_TARGET_OS").expect("target OS not specified");
+
     // path to source.
     let source = PathBuf::from("sentry-native");
+    let backend = get_backend(&target_os);
+
     // path to installation or to install to
     let install = if let Some(install) = env::var_os("SENTRY_NATIVE_INSTALL").map(PathBuf::from) {
         if fs::read_dir(&install)
@@ -31,12 +76,12 @@ fn main() -> Result<()> {
             .and_then(|mut dir| dir.next())
             .is_none()
         {
-            build(&source, Some(&install))?
+            build(&source, Some(&install), backend)?
         } else {
             install
         }
     } else {
-        build(&source, None)?
+        build(&source, None, backend)?
     };
 
     println!("cargo:rerun-if-env-changed=SENTRY_NATIVE_INSTALL");
@@ -62,26 +107,15 @@ fn main() -> Result<()> {
     );
     println!("cargo:rustc-link-lib=sentry");
 
-    match env::var("CARGO_CFG_TARGET_OS")
-        .expect("target OS not specified")
-        .as_str()
-    {
-        crashpad if crashpad == "windows" || crashpad == "macos" => {
+    match backend {
+        Backend::Crashpad => {
             println!("cargo:rustc-link-lib=crashpad_client");
             println!("cargo:rustc-link-lib=crashpad_util");
             println!("cargo:rustc-link-lib=mini_chromium");
 
-            let handler = if crashpad == "windows" {
-                println!("cargo:rustc-link-lib=dbghelp");
-                println!("cargo:rustc-link-lib=shlwapi");
-                println!("cargo:rustc-link-lib=winhttp");
-
+            let handler = if target_os == "windows" {
                 "crashpad_handler.exe"
             } else {
-                println!("cargo:rustc-link-lib=framework=Foundation");
-                println!("cargo:rustc-link-lib=curl");
-                println!("cargo:rustc-link-lib=dylib=c++");
-
                 "crashpad_handler"
             };
 
@@ -90,15 +124,37 @@ fn main() -> Result<()> {
                 install.join("bin").join(handler).display()
             );
         }
-        "linux" => {
+        Backend::Breakpad => {
             println!("cargo:rustc-link-lib=breakpad_client");
+        }
+        _ => {}
+    }
 
+    match target_os.as_str() {
+        "windows" => {
+            println!("cargo:rustc-link-lib=dbghelp");
+            println!("cargo:rustc-link-lib=shlwapi");
+
+            if cfg!(feature = "default-transport") {
+                println!("cargo:rustc-link-lib=winhttp");
+            }
+        }
+        "macos" => {
+            println!("cargo:rustc-link-lib=framework=Foundation");
+            println!("cargo:rustc-link-lib=dylib=c++");
+
+            if cfg!(feature = "default-transport") {
+                println!("cargo:rustc-link-lib=curl");
+            }
+        }
+        "linux" => {
             if cfg!(feature = "default-transport") {
                 println!("cargo:rustc-link-lib=curl");
             }
 
             println!("cargo:rustc-link-lib=dylib=stdc++");
         }
+        "android" => {}
         other => unimplemented!("target platform {} not implemented", other),
     }
 
@@ -106,7 +162,7 @@ fn main() -> Result<()> {
 }
 
 /// Build `sentry_native` with `CMake`.
-fn build(source: &Path, install: Option<&Path>) -> Result<PathBuf> {
+fn build(source: &Path, install: Option<&Path>, backend: Backend) -> Result<PathBuf> {
     let mut cmake_config = Config::new(source);
     cmake_config
         .define("BUILD_SHARED_LIBS", "OFF")
@@ -122,6 +178,15 @@ fn build(source: &Path, install: Option<&Path>) -> Result<PathBuf> {
     if cfg!(not(feature = "default-transport")) {
         cmake_config.define("SENTRY_TRANSPORT", "none");
     }
+
+    let be = match backend {
+        Backend::Crashpad => "crashpad",
+        Backend::Breakpad => "breakpad",
+        Backend::Inproc => "inproc",
+        Backend::None => "none",
+    };
+
+    cmake_config.define("SENTRY_BACKEND", be);
 
     if cfg!(target_feature = "crt-static") {
         cmake_config.define("SENTRY_BUILD_RUNTIMESTATIC", "ON");
